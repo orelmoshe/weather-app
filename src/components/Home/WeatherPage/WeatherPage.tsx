f8cb6f1388e2756ff61eb855b46fc8bbb5d8e8dd
orelmoshe <orel@spectory.com>
2019-12-26 20:03:46 +0200

    level1

--- src/components/Home/WeatherPage/WeatherPage.tsx
+++ src/components/Home/WeatherPage/WeatherPage.tsx
@@ -1,20 +1,18 @@
 import * as React from 'react';
 import { Container,SearchInputContainer,SearchInput,IconSearch } from './WeatherPage.styles';
-import images from '../../../assets/images/images';
+import Images from '../../../assets/images/images';
 import {pageWeatherText} from '../../../consts/text.const';
 import ResultSearch from './ResultSearch/ResultSearch';
-
 interface WeatherPageProps {
 
 }
 
 const WeatherPage = ({ }: WeatherPageProps) => {
-
-
+    
 	return (
 		<Container>
            <SearchInputContainer>
-               <IconSearch src={images.search}></IconSearch>
+               <IconSearch src={Images.search}></IconSearch>
                <SearchInput placeholder={pageWeatherText.ENTER_ITY}/>
            </SearchInputContainer>
 
