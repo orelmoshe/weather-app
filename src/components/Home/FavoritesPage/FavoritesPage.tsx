47f3af6cfd97286c05e3442985c534aa04cde5b2
orelmoshe <orel@spectory.com>
2019-12-29 16:06:25 +0200

    orel

--- src/components/Home/FavoritesPage/FavoritesPage.tsx
+++ src/components/Home/FavoritesPage/FavoritesPage.tsx
@@ -11,46 +11,23 @@ interface FavoritesPageProps {
 }
 
 const FavoritesPage = ({myFavoritesCitysRedux}: FavoritesPageProps) => {
-	const [ myFavoritesCityState, setMyFavoritesCitysState] = useState([])
+	const [ myFavoritesCityState, setMyFavoritesCitysState] = useState([]);
    useEffect(()=>{
-      setMyFavoritesCitysState([{
-         LocalizedName: 'orel',
-         temperature:'31',
-         iconWeather: 'cloudy'
-      },
-      {
-         LocalizedName: 'orel',
-         temperature:'31',
-         iconWeather: 'cloudy'
-      },
-      {
-         LocalizedName: 'orel',
-         temperature:'31',
-         iconWeather: 'cloudy'
-      },{
-         LocalizedName: 'orel',
-         temperature:'31',
-         iconWeather: 'cloudy'
-      },{
-         LocalizedName: 'orel',
-         temperature:'31',
-         iconWeather: 'cloudy'
-      }]);
 	myFavoritesCitysRedux &&
 	getFavoriteCitysDetails(myFavoritesCitysRedux).then(res=>{
 		setMyFavoritesCitysState(res);
-		console.log('dd', myFavoritesCityState)
 	})
    },[]);
 	return (
 		<Container>
-            {myFavoritesCityState &&
+            { myFavoritesCitysRedux &&
                myFavoritesCityState.map((item,index)=>{
+                  console.log('item',item)
                    return <InformationWeatherItem 
-                                key={`InformationWeatherItem${index}`}
+                                key={`InformationWeatherItem_${index}`}
                                 name={item.LocalizedName}
-                                degrees={item.temperature}
-                                textWeather={item.iconWeather}
+                                degrees={'item.temperature'}
+                                textWeather={'item.iconWeather'}
                                 width='190px'
                                 height='250px'
                                 checked={true}
