df23cde076868f29f10804ebec1b0eb3413de58f
orelmoshe <orel@spectory.com>
2019-12-29 14:46:48 +0200

    orel

--- src/components/Home/FavoritesPage/FavoritesPage.styles.ts
+++ src/components/Home/FavoritesPage/FavoritesPage.styles.ts
@@ -1,8 +1,9 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  /* height: calc(100% - 70px - 30px - 30px); */
   width: calc(100% - 30px - 30px);
+  max-width: 1050px;
+  margin: auto;
   display: flex;
   flex-wrap:wrap;
   padding: 30px;
