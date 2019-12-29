eaf2d7ad5a12e1d292eae7db7ce4c6917ea6f79f
orelmoshe <orel@spectory.com>
2019-12-29 14:30:57 +0200

    orel

--- src/services/dataAdapter.service.ts
+++
@@ -1,8 +0,0 @@
-import _ from 'lodash';
-
-// export const incubatorInfoAdapter = (current): IncubatorInfoInterface => {
-// 	return {
-// 		incubator: incubatorAdapter(current.Incubator),
-// 		trolleys: _.map(current.TrolleysVm, t => trolleyVmAdapter(t)),
-// 	};
-// };
