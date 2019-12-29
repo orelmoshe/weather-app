fa2d71eedf32a0deaf89da5e29a6369999bb73c9
orelmoshe <orel@spectory.com>
2019-12-29 12:13:14 +0200

    orel

--- src/services/data.service.ts
+++ src/services/data.service.ts
@@ -2,7 +2,7 @@ import AjaxService from './ajax.service';
 import _ from 'lodash';
 /*AGtT3X7NHATH97gYUwMC9Oj6cVn5sk8G *//*XGgsVXXQGkY6xcFFypdRUmVJHktvZarE - mekory *//*3EJpKcLSBzrz4AQxGQlD6eAs3I71x29s */
 const prefix = 'http://dataservice.accuweather.com';
-const apikey = '8Rzscw8fpsS62ZiAGcxn8ks3iL8HRPzv';
+const apikey = 'sfMnWo3qvk3juneOb76NeuLw5yQIXkwD';
 const ApiRoutes = {
 	AUTOCOMPLETE_SEARCH: `${prefix}/locations/v1/cities/autocomplete`,
 	CURRENT_CONDITIONS:   `${prefix}/currentconditions/v1/`,
